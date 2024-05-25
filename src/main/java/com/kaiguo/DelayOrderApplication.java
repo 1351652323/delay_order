93e3e1a11ca700b4bf3a3b871d8e5139883f8ffe
1351652323 <1351652323@qq.com>
2024-05-25 19:29:46 +0800

    feat: 初始化项目

---
+++ src/main/java/com/kaiguo/DelayOrderApplication.java
@@ -0,0 +1,11 @@
+package com.kaiguo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelayOrderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DelayOrderApplication.class);
+    }
+}
